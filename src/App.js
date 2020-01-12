89f954e8ff5c41e03efd26a8e1da9d9b6f46a28f
runandrerun <the.asantiagojr@gmail.com>
2020-01-12 14:05:21 -0500

    Setup create-react-app. Setup Sass. Added Homepage with basic template.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Homepage from './components/homepage/Homepage.component';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
